8e7de585c002db177f8c195d745ec840e9a73030
Alan Magliolini <alannm54@gmail.com>
2023-09-18 11:23:59 -0300

    finish function singleRound

--- rock_papers_scissors.js
+++ rock_papers_scissors.js
@@ -1,10 +1,27 @@
 console.log("HOLA");
 
 let elements = ["Rock", "Paper" , "Scissors"]
-
+let elementUser = prompt("Rock,Paper or Scissors?")
 function getComputerChoice(){
     const indiceAleatorio = Math.floor(Math.random() * elements.length);
+    
     return elements[indiceAleatorio]
 }
 
-console.log(getComputerChoice())
+
+function singleRound(playerSelection,computerSelection){
+      if(playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Scissors" && computerSelection === "Rock"){
+        console.log(playerSelection,computerSelection);
+        return "Rock Wins!"
+      }else if(playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Rock"){
+        console.log(playerSelection,computerSelection);
+        return "Paper Wins!"
+      }else if(playerSelection === "Scissors" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors"){
+        console.log(playerSelection,computerSelection);
+        return "Scissors Wins!"
+      }
+      else{
+        return "Empate!"
+      }
+}
+console.log(singleRound(elementUser,getComputerChoice()))
