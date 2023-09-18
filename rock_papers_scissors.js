8d16c94b75316643bdde977256552bf2401dbe10
Alan Magliolini <alannm54@gmail.com>
2023-09-18 00:36:26 -0300

    add function getComputerChoice

--- rock_papers_scissors.js
+++ rock_papers_scissors.js
@@ -1,1 +1,10 @@
 console.log("HOLA");
+
+let elements = ["Rock", "Paper" , "Scissors"]
+
+function getComputerChoice(){
+    const indiceAleatorio = Math.floor(Math.random() * elements.length);
+    return elements[indiceAleatorio]
+}
+
+console.log(getComputerChoice())
